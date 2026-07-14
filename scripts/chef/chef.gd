9942fbef2f56e28ca68086034c3c5a3a79b366af
Danilo <eggwardchickfila@gmail.com>
2026-07-15 01:21:53 +0200

    chganges

--- scripts/chef/chef.gd
+++ scripts/chef/chef.gd
@@ -1,6 +1,5 @@
 extends CharacterBody2D
 
-@onready var target_position = global_position
 @onready var nav_agent: NavigationAgent2D = $NavAgent2d
 var speed = 40.0
 var direction = Vector2(0.0,0.0)
@@ -10,12 +9,13 @@ func _physics_process(delta: float) -> void:
 	var direction = (next_point - global_position).normalized()
 	velocity = direction * speed * delta
 	
-	var distance = global_position.distance_to(target_position)
+	var distance = global_position.distance_to(nav_agent.target_position)
 	if distance < 2.5:
 		velocity = Vector2.ZERO
-		
+		nav_agent.target_position = Vector2(int(nav_agent.target_position[0]),
+		int(nav_agent.target_position[1]))
 	move_and_collide(velocity)
 
 func give_target(given_pos):
-	target_position = given_pos
-	nav_agent.target_position = given_pos
+	var pos = Vector2(int(given_pos[0]),int(given_pos[1]))
+	nav_agent.target_position = pos
--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
+const TARGET_SPRITE = preload("res://assets/vfx/target_sprite.tscn")
 @onready var hitbox: Area2D = $Hitbox
-@onready var target_sprite: AnimatedSprite2D = $TargetSprite
 var selected_object = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,12 +13,15 @@ func _process(delta: float) -> void:
 		select()
 	if Input.is_action_just_pressed("Mouse2"):
 		place()
-	target_sprite.visible = false
 	if selected_object:
-		target_sprite.visible = true
-		target_sprite.position = (selected_object.position - global_position) + Vector2(0,-12) # png offset
+		if !selected_object.has_node("TargetSprite"):
+			var tar = TARGET_SPRITE.instantiate()
+			selected_object.add_child(tar)
 
 func select():
+	if selected_object:
+		var tar = selected_object.get_node("TargetSprite")
+		tar.queue_free()
 	selected_object = null # Deselect object when no object is found
 	var collisions = hitbox.get_overlapping_bodies()
 	for obj in collisions:
@@ -27,7 +30,7 @@ func select():
 			break
 		if obj.is_in_group("dish"): # Dish checked after chefs are. To get info about dish
 			break
-			
+
 func place():
 	if selected_object:
 		selected_object.give_target(global_position)
