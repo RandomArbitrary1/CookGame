a472b8c6d7bd0253ec716634e06a7662caf2496e
mkaiast <itsmkaiast@gmail.com>
2026-07-14 03:46:28 +0200

    ui wip

---
+++ assets/ui/button.gd
@@ -0,0 +1,17 @@
+extends Button
+
+
+func _ready() -> void:
+	mouse_entered.connect(func():
+		get_node("Outline").visible = true
+	)
+	mouse_exited.connect(func():
+		get_node("Outline").visible = false
+		self_modulate = Color(1,1,1)
+	)
+	button_down.connect(func():
+		self_modulate = Color(.6,.6,.6)
+	)
+	button_up.connect(func():
+		self_modulate = Color(1,1,1)
+	)
