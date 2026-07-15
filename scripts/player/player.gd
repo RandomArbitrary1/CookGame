32964fdd9744b9bb81e0599bd122ce36f12ad58b
Danilo <eggwardchickfila@gmail.com>
2026-07-15 02:27:45 +0200

    sdfaasdf

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 const TARGET_SPRITE = preload("res://assets/vfx/target_sprite.tscn")
 @onready var hitbox: Area2D = $Hitbox
+@onready var grid_highlight: AnimatedSprite2D = $GridHighlight
 var selected_object = null
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,6 +18,7 @@ func _process(delta: float) -> void:
 		if !selected_object.has_node("TargetSprite"):
 			var tar = TARGET_SPRITE.instantiate()
 			selected_object.add_child(tar)
+	calculate_grid_highlight()
 
 func select():
 	if selected_object:
@@ -29,8 +31,13 @@ func select():
 			selected_object = obj
 			break
 		if obj.is_in_group("dish"): # Dish checked after chefs are. To get info about dish
+			print(obj)
 			break
 
 func place():
 	if selected_object:
 		selected_object.give_target(global_position)
+		
+func calculate_grid_highlight():
+	grid_highlight.global_position = global_position.snapped(Vector2(16, 16))
+	grid_highlight.global_position += Vector2(8,8) # a crappy offset
